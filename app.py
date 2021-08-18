ee4ec164fed0bdb8ba1ef663f88296d15eed896d
Goutham Nannuta <69679486+gouthamNannuta@users.noreply.github.com>
2021-08-18 15:03:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,52 @@
-from flask import Flask, jsonify, request
+from flask import Flask,jsonify, request
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello_msg():
-    return "Hello Mam !!"
-
-tasks = [
+List = [
     {
-        "id":1,
-        "title":"Get up at 5 am",
-        "description":"Teach the morning 6 am ",
-        "done":False
+        'id': 1,
+        'Name': 'koushik',
+        'Contact': '9014873628', 
+        'done': False
     },
-        {
-        "id":2,
-        "title":"Sleep at 7 am",
-        "description":"sleep after teaching the 6 am for 1 Hour",
-        "done":False
-    },
-        {
-        "id":3,
-        "title":"Get up at 8 am",
-        "description":"Teach the coding clases agian ",
-        "done":False
-    },
-    
+    {
+        'id': 2,
+        'Name': 'rishik',
+        'Contact': '9666422183', 
+        'done': False
+    }
 ]
 
-@app.route("/get-data")
-def get_task():
-    return jsonify({
-        "data":tasks
-    })
+@app.route("/")
+def hello_world():
+    return "Hello World!"
 
-@app.route("/add-data",methods=["POST"])
+@app.route("/add-data", methods=["POST"])
 def add_task():
     if not request.json:
         return jsonify({
             "status":"error",
-            "message":"please provide JSON data"
+            "message": "Please provide the data!"
+        },400)
 
-        }, 400)
-    task = {
-        "id": tasks[-1]["id"]+1,
-        "title": request.json["title"],
-        "description": request.json.get("description",""),
-        "done":False
+    contact = {
+        'id': tasks[-1]['id'] + 1,
+        'Name': request.json['Name'],
+        'Contact': request.json.get('Contact', ""),
+        'done': False
     }
-    tasks.append(task)
+    List.append(contact)
     return jsonify({
         "status":"success",
-        "message":"Task added successfully !!"
+        "message": "Contact added succesfully!"
     })
+    
 
-if(__name__=="__main"):
+@app.route("/get-data")
+def get_task():
+    return jsonify({
+        "data" : List
+    }) 
+
+if (__name__ == "__main__"):
     app.run(debug=True)
